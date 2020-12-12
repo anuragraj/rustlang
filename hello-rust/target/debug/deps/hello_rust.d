ff03cf7cf740d3335d95d7ce084df5cbb7ac0674
anuragraj <go2anurag@yahoo.com>
2020-12-12 20:14:01 +0530

    Code Rusting!

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+D:\Documents\rust\rustlang\hello-rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+D:\Documents\rust\rustlang\hello-rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+D:\Documents\rust\rustlang\hello-rust\target\debug\hello-rust.exe: D:\Documents\rust\rustlang\hello-rust\src\main.rs
