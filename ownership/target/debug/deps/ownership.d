b40ce6bf8a7fc101d9142b7f88c0d3e31bcc8b53
anuragraj <go2anurag@yahoo.com>
2020-12-13 22:31:39 +0530

    Code Rusting!

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+D:\Documents\rust\rustlang\ownership\target\debug\deps\ownership.exe: src\main.rs
+
+D:\Documents\rust\rustlang\ownership\target\debug\deps\ownership.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+D:\Documents\rust\rustlang\ownership\target\debug\ownership.exe: D:\Documents\rust\rustlang\ownership\src\main.rs
